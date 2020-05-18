7f43931e221e310af896956cadec73dd85168d7d
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-05-18 19:24:52 +0700

    removed nodes to root

--- nodejs/runnode.go
+++ nodejs/runnode.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ExecScreenshot(url string) string {
-	command := "node node/screenshot.js " + url
+	command := "node screenshot.js " + url
 	parts := strings.Fields(command)
 	data, err := exec.Command(parts[0], parts[1:]...).Output()
 	if err != nil {
