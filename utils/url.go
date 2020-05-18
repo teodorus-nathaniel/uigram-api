d517f92d6f434b45f1caa3650fd6a9e08fa1f871
TeodorusNathaniel <nathankurnia26@gmail.com>
2020-05-18 18:49:11 +0700

    added wait for ss

--- utils/url.go
+++ utils/url.go
@@ -1,4 +1,5 @@
 package utils
 
-// var URL string = "https://uigram-api.herokuapp.com/"
-var URL string = "http://localhost:8080/"
+var URL string = "https://uigram-api.herokuapp.com/"
+
+// var URL string = "http://localhost:8080/"
